613713a6d794330ddf60e195382eb2dffdfee48c
EvgeNia2021 <86658826+EvgeNia2021@users.noreply.github.com>
2021-09-06 20:45:35 +0300

    Add files via upload

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,4 +1,4 @@
-const slider = $(".products").bxSlider({
+/*const slider = $(".products").bxSlider({
 pager:false,
 controls:false,
 });
@@ -11,4 +11,52 @@ $(".products-slider__arrow--prev").click((e) => {
 $(".products-slider__arrow--next").click((e) => {
   e.preventDefault();
   slider.goToNextSlide();
+});*/
+
+
+const left = document.querySelector("#left");
+const right = document.querySelector("#right");
+const itemsList = document.querySelector("#items");
+
+
+
+const loop = (direction, e) => {
+  e.preventDefault();
+
+  if (direction === "right") {
+    itemsList.appendChild(itemsList.firstElementChild);
+   
+  } else { 
+    itemsList.insertBefore(itemsList.lastElementChild, items.firstElementChild);
+   
+  }
+};
+
+right.addEventListener("click", (e) => {
+ loop("right", e);
+
 });
+
+left.addEventListener("click", (e) => {
+ loop("left", e);
+ 
+ });
+
+ /*var productsItem = document.querySelector(".products__item");
+
+ left.addEventListener("click", addclass)
+ function addclass() {
+   if (menu.classList.contains("activeSlide")) {
+     menu.classList.remove("activeSlide");
+ 
+     
+     
+   } else {
+     menu.classList.add("activeSlide");
+   
+   }
+ 
+ 
+ };
+ */
+ 
