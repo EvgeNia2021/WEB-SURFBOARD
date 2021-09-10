e1cb842bb306c2db84374e995dd7708ed58f9f5a
EvgeNia2021 <86658826+EvgeNia2021@users.noreply.github.com>
2021-09-10 16:33:26 +0300

    before gulp

--- scripts/map.js
+++ scripts/map.js
@@ -1,22 +1,19 @@
 let myMap;
 const init = () => {
  myMap = new ymaps.Map("map", {
-   center: [59.93916998692174, 30.309015096732622],
-   zoom: 11,
+   center: [55.75198619547685, 37.6072381401977],
+   zoom: 14,
    controls: [],
  });
  
  let coords = [
-     [59.94554327989287, 30.38935262114668],
-     [59.91142323563909, 30.50024587065841],
-     [59.88693161784606, 30.319658102103713],
-     [59.97033574821672, 30.315194906302924],
+  [55.74963840542429,37.60408386239617],
    ],
    myCollection = new ymaps.GeoObjectCollection({}, {
      draggable: false,
      iconLayout: 'default#image',
-     iconImageHref: './img/icons/marker.svg',
-     iconImageSize: [46, 57],
+     iconImageHref: './img/icons/mark.svg',
+     iconImageSize: [58, 73],
      iconImageOffset: [-35, -52]
    });
  
